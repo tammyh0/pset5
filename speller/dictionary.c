1b6f58d73cb689803cd184c7d5e02d9a64b8a3bb
Tammy Ho <tammyhho98@gmail.com>
2021-03-16 18:38:39 +0000

    Test

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -42,7 +42,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
+    // TODO testtest
     return 0;
 }
 
